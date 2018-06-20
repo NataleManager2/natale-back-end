1e346858a3afa6c916892acdec4c9579666a1ed9
Tomás <tomas.peirotti@gmail.com>
2018-06-20 20:14:39 -0300

    Update schema

--- scripts/Export MySQL DB.bat
+++ scripts/Export MySQL DB.bat
@@ -1,2 +1,2 @@
 cd C:\Program Files\MySQL\MySQL Server 5.7\bin
-.\mysqldump.exe -u root -p --databases natale >C:\Users\tomas\Documents\natale.sql
+.\mysqldump.exe -u root -p --databases natale >C:\Users\tomas\Documents\GitHub\natale-back-end\scripts\nataleSchema.sql
