84a0f0cbe5ee9a50bdb18e73c4861a631ad05fb9
Tomás <tomas.peirotti@gmail.com>
2018-10-16 00:50:48 -0300

    Security update

--- src/main/java/com/natale/nataleManager/config/SecurityConfig.java
+++ src/main/java/com/natale/nataleManager/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.natale.nataleManager.config;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,12 +21,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // Add here any custom code you need in order to get the credentials from the user...
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("USER");
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        final User.UserBuilder userBuilder = User.builder().passwordEncoder(encoder::encode);
+        UserDetails user = userBuilder
+                .username("natale")
+                .password("normaluser")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = userBuilder
+                .username("admin")
+                .password("nataleadmin")
+                .roles("USER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
