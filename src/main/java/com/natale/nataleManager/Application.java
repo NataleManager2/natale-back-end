1969cc9456cb7db163896d3409b94b0776a0d553
Tomás <tomas.peirotti@gmail.com>
2018-10-20 17:01:12 -0300

    db.json created for json-server

--- src/main/java/com/natale/nataleManager/Application.java
+++ src/main/java/com/natale/nataleManager/Application.java
@@ -1,31 +1,15 @@
 package com.natale.nataleManager;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-/*
- * This is the main Spring Boot application class. It configures Spring Boot, JPA, Swagger
- */
 @SpringBootApplication
-@EnableJpaRepositories("com.natale.nataleManager.repository") // To segregate MongoDB and JPA repositories. Otherwise not needed.
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     private static final Class<Application> applicationClass = Application.class;
-    private static final Logger log = LoggerFactory.getLogger(applicationClass);
 
 	public static void main(String[] args) {
 		SpringApplication.run(applicationClass, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
-    
 
 }
